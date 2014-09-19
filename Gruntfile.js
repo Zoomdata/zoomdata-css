d59c0c95174ff5329e9348a2956d8a93e08a5dd4
William Lindner <wlindner522@gmail.com>
2014-09-19 12:12:28 -0700

    added sass, cssmin, watch and autoprefixer for building stylesheets.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,24 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true,
+                outputStyle: 'nested' //sass sucks at minification, don't use it.
+            },
+            dist: {
+                files: {
+                    'style/main.css': 'style/main.scss'
+                }
+            }
+        },
+        cssmin: {
+            combine: {
+                files: {
+                    'style/main.css': ['style/main.css']
+                }
+            }
+        },
         kss: {
             options: {
                 includeType: 'scss',
@@ -12,13 +30,36 @@ module.exports = function(grunt) {
                     'styleguide': ['style']
                 }
             }
+        },
+        autoprefixer: {
+            dist: {
+                files: {
+                    'style/main.css': 'style/main.css'
+                }
+            }
+        },
+        watch: {
+            kss: {
+                files: ['style/**/*.scss', 'style/styleguide.md'],
+                tasks: ['kss']
+            }
         }
     });
 
+    grunt.loadNpmTasks('grunt-sass');
     grunt.loadNpmTasks('grunt-kss');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // A very basic default task.
     grunt.registerTask('default', [
-        'kss'
+        'watch'
+    ]);
+
+    grunt.registerTask('pre', [
+        'sass',
+        'autoprefixer',
+        'cssmin'
     ]);
 };
